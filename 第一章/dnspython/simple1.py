c08a418f29403fbed4e1654b1e0b6d298b8bef37
zqadm <jzq_pohs@sina.com>
2019-02-22 10:29:10 +0800

    20190222

--- 第一章/dnspython/simple1.py
+++ 第一章/dnspython/simple1.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import dns.resolver
+from past.builtins import raw_input
 
 domain = raw_input('Please input an domain: ')
 
 A = dns.resolver.query(domain, 'A')
 for i in A.response.answer:
     for j in i.items:
-        print j.address
+        print(j.address)
 
